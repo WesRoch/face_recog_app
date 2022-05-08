5422a686496fc127c76bd1d89c0e550979d3e83d
WesRoch <wesleyroodrigo@gmail.com>
2022-05-08 12:26:12 -0300

    first changes on login_screen

---
+++ lib/login/login_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: GestureDetector(
+          child: Stack(
+            children: <Widget>[
+              Container(
+                height: double.infinity,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                      Color(0xffbae1ff),
+                      Color(0xffbae1ff),
+                      Color(0xffbae1ff),
+                      Color(0xffbae1ff),
+                    ])),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
