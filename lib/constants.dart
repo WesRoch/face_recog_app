b66360b06d27415ec71a5dfe113bfa71e2b92912
WesRoch <wesleyroodrigo@gmail.com>
2022-06-11 16:24:38 -0300

    overall changes

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,7 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xffbae1ff);
 const kPrimaryLightColor = Color(0x66bae1ff);
+
+const APP_NAME = "FaceRecog";
+var APP_BAR_COLOR = Colors.blue[800];
+var APP_BAR_GRADIENT_COLOR = <Color>[Colors.blue[800], Colors.blue];
