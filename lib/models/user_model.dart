936d361e09070e96851f9d16415f09f2f21dc92b
WesRoch <wesleyroodrigo@gmail.com>
2022-06-11 16:27:08 -0300

    added user model

---
+++ lib/models/user_model.dart
@@ -0,0 +1,31 @@
+class Usuario {
+  Usuario({
+    this.userId,
+    this.userName,
+    this.userEmail,
+    this.userPassword,
+    this.avatar,
+  });
+
+  int userId;
+  String userName;
+  String userEmail;
+  String userPassword;
+  String avatar;
+
+  factory Usuario.fromJson(Map<String, dynamic> json) => Usuario(
+        userId: json["id"],
+        userName: json["name"],
+        userEmail: json["email"],
+        userPassword: json["password"],
+        avatar: json["avatar"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": userId,
+        "name": userName,
+        "email": userEmail,
+        "password": userPassword,
+        "avatar": avatar,
+      };
+}
